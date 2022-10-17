90db3c963b38230fc4e360da1dcf839b1c995998
ManassehG <manasseh65mwas25@gmail.com>
2022-10-17 01:44:26 -0700

    Low level programming using C

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - starting point of code
+ * Return: always 0 on success
+ * void - no return value
+ */
+
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %i ", n);
+	lastDigit = n % 10;
+	printf("is %d and ", lastDigit);
+	if (lastDigit > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (lastDigit == 0)
+	{
+		printf("and is 0\n");
+	}
+	else if (lastDigit < 6 && lastDigit != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
