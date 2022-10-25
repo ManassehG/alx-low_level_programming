482c1f58d2c7630367d393677dc998d9fa2796b8
ManassehG <manasseh65mwas25@gmail.com>
2022-10-25 04:03:12 -0700

    Pointers and arrays in functions

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - determines length of a string
+ * @s: input number
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int coun;
+
+	coun = strlen(s);
+	return (coun);
+}
