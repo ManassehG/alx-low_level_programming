fb9b513d073380bfcdb6aa28a9ade01f4a1cda91
ManassehG <manasseh65mwas25@gmail.com>
2022-10-25 12:55:18 -0700

    Pointers and arrays in functions

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,28 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts_half - prints second half of an input
+ * @str: input
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int a, length, n;
 
+	length = strlen(str);
+
+	if (length % 2 == 0)
+	{
+		n = length / 2;
+	}
+	else
+	{
+		n = (length - 1) / 2;
+	}
+	for (a = n; a < length; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
