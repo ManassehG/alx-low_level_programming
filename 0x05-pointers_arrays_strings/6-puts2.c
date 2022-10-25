d6c1610657ce19e70230a98acb6e6cb8bfee418a
ManassehG <manasseh65mwas25@gmail.com>
2022-10-25 07:52:03 -0700

    Pointers and arrays in functions

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts2 - prints every other number
+ * @str: Input
+ *
+ * Return: every other number in twos
+ */
+void puts2(char *str)
+{
+	int a, length;
+
+
+	length = strlen(str);
+	for (a = 0; a < length; a += 2)
+	{
+		_putchar(str[a]);
+	}
+}
