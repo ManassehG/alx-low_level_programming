979956b6b76a378f8c5ed1f438c23619ecefb520
ManassehG <manasseh65mwas25@gmail.com>
2022-10-29 10:29:39 -0700

    working with more pointers and arrays

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - concentrates strings
+ * @dest: input
+ * @src: input
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}
