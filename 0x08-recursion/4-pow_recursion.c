687f64c729cff048d44ab0f53db49113662fc658
ManassehG <manasseh65mwas25@gmail.com>
2022-11-03 05:36:17 -0700

    Recursions in C

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include "main.h"
+/**
+ * _pow_recursion - prints x to power y
+ * @x: input
+ * @y: input
+ * Return: x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (pow(x, y));
+}
