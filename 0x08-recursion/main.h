5423630c4ebb7a1ae20b140fd54e59eac3f85776
ManassehG <manasseh65mwas25@gmail.com>
2022-11-03 06:29:05 -0700

    Recursions in C

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,8 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _pow_recursion(int x, int y);
+int _sqrt(int c, int i);
+int _sqrt_recursion(int n);
 
 
 
