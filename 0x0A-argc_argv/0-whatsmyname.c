acc4928046737dea0903f224e7f65bea22f51dde
ManassehG <manasseh65mwas25@gmail.com>
2022-11-12 11:38:32 -0800

    Working with argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Return: 0
+ * @argc: count for the arguements
+ * @argv: one dimensional vector
+ */
+
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
