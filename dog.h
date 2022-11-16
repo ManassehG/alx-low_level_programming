4ea44cd950b90fea88e6a05c24a2311ebf18ab09
ManassehG <manasseh65mwas25@gmail.com>
2022-11-16 08:46:40 -0800

    Working with structures and typedef

--- dog.h
+++ dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - contains details of dog
@@ -17,5 +17,15 @@ struct dog
 	char *owner;
 
 };
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-#endif /* MAIN_H */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
